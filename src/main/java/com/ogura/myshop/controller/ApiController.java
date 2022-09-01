43aaddcf3b35b76ff197617cefa82b405c6332ac
yutampos <>
2022-09-01 12:18:40 +0900

    port cross設定

--- src/main/java/com/ogura/myshop/controller/ApiController.java
+++ src/main/java/com/ogura/myshop/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.ogura.myshop.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import com.ogura.myshop.entity.item.ItemImages;
 import com.ogura.myshop.service.OguraService;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class ApiController {
 
